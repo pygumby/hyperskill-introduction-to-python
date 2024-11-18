c4b75d7680ff1308520f120eaace71b766bce6f8
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2024-11-18 14:00:26 +0100

    Add "Taking user's input"

---
+++ Topics/Taking user's input/Coding from scratch/main.py
@@ -0,0 +1,5 @@
+# Take the user's nickname as input
+name = input()
+
+# Print the welcome message
+print(f"Welcome back {name}!")
---
+++ Topics/Taking user's input/Final boss/main.py
@@ -0,0 +1,8 @@
+# Imput user's name
+name = input()
+
+# Input user's profession
+job = input()
+
+# Print the message
+print(f"My name is {name} and I am a {job}")
