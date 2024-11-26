d15f31992323832962708df887c13e5488c8e31f
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2024-11-26 16:59:19 +0100

    Add "Program with numbers"

---
+++ Topics/Program with numbers/Calculating an expression/main.py
@@ -0,0 +1,5 @@
+# Take input
+n = int(input())
+
+# Calculate the expression and print the result
+print(((n + 1) * n + 2) * n + 3)
---
+++ Topics/Program with numbers/Calculating volume/main.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Calculate the volume and print it
+print(a * b * c)
