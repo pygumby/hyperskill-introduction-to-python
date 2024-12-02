cd0f7b5c3fc8a6c1232d6ab04c16f0218f775801
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2024-12-02 14:55:38 +0100

    Add "If statement"

---
+++ Topics/If statement/Check if a number falls within a specific range/main.py
@@ -0,0 +1,11 @@
+# You can get the input number as follows
+input_number = int(input())
+
+# Write your if statement for number range here
+is_in_range = 10 <= input_number <= 20
+
+# Based on the result of your if statement, print the appropriate message
+if is_in_range:
+    print("Inside the range")
+else:
+    print("Outside the range")
