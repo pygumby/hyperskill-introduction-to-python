24dcdf6418b80cb326a156b2f6bcb6d6d18e945b
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2025-01-15 11:36:55 +0100

    Add "Importing modules"

---
+++ Topics/Topics/Importing modules/Calculate the factorial of a given number/main.py
@@ -0,0 +1,8 @@
+# Import the required module
+import math
+
+# Input the number
+n = int(input())
+
+# Use the factorial function from the math module to calculate the factorial
+print(math.factorial(n))
---
+++ Topics/Topics/Importing modules/Not so random/main.py
@@ -0,0 +1,5 @@
+# place `import` statement at top of the program
+import random
+
+# put your code here
+n = random.randint(-100, 100)
