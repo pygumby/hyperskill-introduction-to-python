e443326ea2d45ef3680d59148ecfa870debf0f06
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2025-01-17 10:34:00 +0100

    Add "Errors"

---
+++ Topics/Topics/Errors/Greetings Generator  Dodge the Bug/main.py
@@ -0,0 +1,9 @@
+def greet_user():
+    # Get the user's name from command line input
+    name = input()
+    # Print a greeting message
+    print(f"Hello, {name}!")
+
+# Check if the script is the main entry point
+if __name__ == '__main__':
+    greet_user()
