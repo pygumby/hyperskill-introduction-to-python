81b2a1f597436d01fcb4ff4e745300022c763b06
Lucas Konstantin Bärenfänger <18075314+pygumby@users.noreply.github.com>
2025-02-15 19:24:57 +0100

    Add "String formatting"

---
+++ Topics/Topics/String formatting/Greeting with full name/main.py
@@ -0,0 +1,9 @@
+# Get the user's first name
+first_name = input()
+
+# Get the user's last name
+last_name = input()
+
+# Print the greeting message
+# Use string formatting to create the greeting
+print(f"Hello, {first_name} {last_name}! Welcome.")
